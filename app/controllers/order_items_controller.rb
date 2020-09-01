50fd8758ba287b6d236ecc46bf08ef43ae46a9a5
Jose <jocvegar@gmail.com>
2020-09-01 06:06:21 -0600

    adds sessions order

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -26,9 +26,8 @@ class OrderItemsController < ApplicationController
   def create
     @order = current_order
     @order_item = @order.order_items.new(order_item_params)
-
     respond_to do |format|
-      if @order_item.save
+      if @order.save
         session[:order_id] = @order.id
         format.html { redirect_to @order_item, notice: 'Order item was successfully created.' }
         format.json { render :show, status: :created, location: @order_item }
