806792e8fc73ad0052f257ff86045c0d097ff67e
Jose <jocvegar@gmail.com>
2020-09-07 08:18:32 -0600

    adds jquery

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,19 @@
 const { environment } = require('@rails/webpacker')
 
+const webpack = require("webpack");
+environment.plugins.prepend(
+  "Provide",
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+  })
+);
+
+const aliasConfig = {
+	'jquery': 'jquery/src/jquery.js',
+	$: 'jquery/src/jquery.js',
+};
+
+environment.config.set('resolve.alias', aliasConfig);
+
 module.exports = environment
