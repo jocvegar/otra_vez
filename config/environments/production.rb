31b25e6c60fc8cd78f7d7890cf05d56eb417bf67
Jose <jocvegar@gmail.com>
2020-09-12 18:29:47 -0600

    changes email domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -114,7 +114,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'example.com',
+    domain:               'otravez-hn.herokuapp.com',
     user_name:            ENV['EMAIL_ADDRESS'],
     password:             ENV['EMAIL_PASSWORD'],
     authentication:       'plain',
