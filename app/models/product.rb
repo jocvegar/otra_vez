0e0d12ec3ba12cf9cad217a7744c7e56353f07e3
Jose <jocvegar@gmail.com>
2020-10-04 15:14:45 -0600

    adds dependence

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ApplicationRecord
 	extend FriendlyId
 	friendly_id :title, use: :slugged
 
-	has_many :order_items
+	has_many :order_items, dependent: :destroy
 	has_one_attached :main_image
 
 	scope :existing, lambda { where("quantity > ?", 0) }
