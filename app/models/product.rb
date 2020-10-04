a2e7418bb434fdaf35ac359fe9cf51faff790a3c
Jose <jocvegar@gmail.com>
2020-10-04 16:17:14 -0600

    adds unisex

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,8 +7,8 @@ class Product < ApplicationRecord
 
 	scope :existing, lambda { where("quantity > ?", 0) }
 	scope :sold_out, lambda { where("quantity <= ?", 0) }
-	scope :hombre, lambda { where("category = ?", "hombre") }
-	scope :mujer, lambda { where("category = ?", "mujer") }
+	scope :hombre, lambda { where("category = ?", "hombre").or(where("category = ?", "unisex")) }
+	scope :mujer, lambda { where("category = ?", "mujer").or(where("category = ?", "unisex")) }
 	scope :otro, lambda { where("category = ?", "otro") }
 
 	def should_generate_new_friendly_id?
@@ -21,5 +21,9 @@ class Product < ApplicationRecord
 
 	def self.sizes
 	  return %w(xxs xs s m l xl fits_all)
+	end
+
+	def self.categories
+		return %w(hombre mujer unisex otro)
 	end
 end
